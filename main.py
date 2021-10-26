95db418352c89cdd61167e9281d4c14736d20d57
sentient-codebot-nan <linnunium@live.com>
2021-10-26 15:38:42 +0200

    lr bug fix (un-elegant way)

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def train_model(model, epochs, train_data, val_data):
 		v_accuracy = test_model(model, val_data) # not yet revised
 		# v_accuracy2 = test_model(model, val_data, data.val_get2)
 		# v_accuracy3 = test_model(model, val_data, data.val_get3)
-		optimizer.lr = optimizer.lr/2.15 # cube root of 10. lr drops to 1/10 every 3 epoch
+		optimizer.defaults["lr"] = optimizer.defaults["lr"]/2.15 # cube root of 10. lr drops to 1/10 every 3 epoch
 		scheduler.step(v_accuracy)
 		
 		print('previous best validation accuracy ' + str(best_acc))
